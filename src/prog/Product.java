55de44f11b58649e5be33490f22c3d18f9df5bde
SeanGit95 <65398393+SeanGit95@users.noreply.github.com>
2020-06-15 19:54:13 +0300

    Update the Product file
    updated the product file with functions

--- src/prog/Product.java
+++ src/prog/Product.java
@@ -17,6 +17,56 @@ public class Product implements Serializable {
         this.price = price;
         this.quantity = quantity;
     }
-	
-	
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public void deductQuantity(int takenQuantity) {
+        this.quantity -= takenQuantity ;
+    }
+
+    public void increaseQuantity(int addedQuantity) {
+        this.quantity += addedQuantity ;
+    }
+
+
+    public String toString(){
+//        return productID +"    "+productName+"    "+price+"    "+quantity;
+        return String.format( "%-10s %-15s %-15s %-15s",productID,productName,price,quantity );
+    }
 }
