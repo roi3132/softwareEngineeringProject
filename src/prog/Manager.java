0f9cf211ebfaf1a9a1a8fc728815a4043ef5159d
bar <barfranko2@gmail.com>
2020-07-07 21:45:44 +0300

    added comments

--- src/prog/Manager.java
+++ src/prog/Manager.java
@@ -1,21 +1,22 @@
 package prog;
 
 public class Manager extends Worker {
-
+    // the constructor for the manager, gets all the details for the manager
     public Manager(String id, String address, Gender gender, String email, String firstName, String lastName, String password, AccessLevel accessLevel, String userName) {
         super(id, address, gender, email, firstName, lastName, password, accessLevel, userName);
         super.isManager=true;
         super.accessLevel= AccessLevel.MANAGER;
     }
 
+
     public void updateSalary(Worker worker,Double updatedSalary){
         worker.setSalary(salary);
     }
+    // other functions that may be used later
+    //public void updateManager(){};
 
-    public void updateManager(){};
+    //public void addWorker(){}
 
-    public void addWorker(){}
-
-    public void deleteWorker(){}
+    //public void deleteWorker(){}
 
 }
