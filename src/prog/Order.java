53c9f3388a17892ddea1968e320eb49e3ec344a7
SeanGit95 <65398393+SeanGit95@users.noreply.github.com>
2020-07-08 17:33:43 +0300

    added more comments to the code

--- src/prog/Order.java
+++ src/prog/Order.java
@@ -13,57 +13,57 @@ public class Order implements Serializable {
     private double totalAmount;
     private boolean isInStock;
     private boolean isPaid;
-
+    // the constructor for the order, creates a new order and adds it to the order list
     public Order(int orderNumber) {
         this.orderNumber = orderNumber;
         this.productArrayList= new ArrayList<Product>();
     }
-
+    // returns the order number
     public int getOrderNumber() {
         return orderNumber;
     }
 
-
+    // returns the list of products in the order
     public ArrayList<Product> getProductArrayList() {
         return productArrayList;
     }
-
+    // set a list of products
     public void setProductArrayList(ArrayList<Product> productArrayList) {
         this.productArrayList = productArrayList;
     }
-
+    // return which worker is taking care of the order
     public String getWorkerTakingCare() {
         return workerTakingCare;
     }
-
+    // setting which worker is taking care of the order
     public void setWorkerTakingCare(String workerTakingCare) {
         this.workerTakingCare = workerTakingCare;
     }
-
+    // return the customer ID for the order
     public String getCustomerId() {
         return customerId;
     }
-
+    // set the customer ID for the order
     public void setCustomerId(String customerId) {
         this.customerId = customerId;
     }
-
+    // returns the address for the order
     public String getAddress() {
         return address;
     }
-
+    // setting the address for the order
     public void setAddress(String address) {
         this.address = address;
     }
-
+    // returns the total amount for the order
     public double getTotalAmount() {
         return totalAmount;
     }
-
+    // set the total amount for the order
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
-
+    // return whether the product is in stock
     public boolean isInStock() {
         return isInStock;
     }
